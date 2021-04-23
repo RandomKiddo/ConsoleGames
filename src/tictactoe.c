0d2784425fccb18377f904fdde3625c3d13b62d9
RandomKiddo <nghugare2@outlook.com>
2021-04-23 19:51:00 -0400

    Started CPU TTT Engine

--- src/tictactoe.c
+++ src/tictactoe.c
@@ -1,28 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #define DIFFICULTY_EASY 1
 #define DIFFICULTY_MEDIUM 2
 #define DIFFICULTY_HARD 3
 
 int gameIsWon(void);
+int gameIsDrawn(void);
 int spotNotAvailable(int number);
 int takeEasyTurn(void);
 int takeMediumTurn(void);
 int takeHardTurn(void);
+int userPlayedACorner(void);
+int userPlayedAnEdge(void);
+int turnBasedOnPlayedCorner(void);
+int edgesOpen(void);
+int cornersOpen(void);
 
 int board[3][3] = { {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1} };
 char str[49] = "1 | 2 | 3\n---------\n4 | 5 | 6\n---------\n7 | 8 | 9";
 int indices = { 0, 4, 8, 20, 24, 28, 40, 44, 48 };
+char userChar, cpuChar;
+int lastMove;
+int turn; // turn 0 = computer, turn 1 = player
 
 int main(void) {
 	char first;
-	char userChar, cpuChar;
 	do {
 		printf("%s", "\nWould you like to go first? y/n: ");
 		scanf("%c", first);
 	} while (first != 'y' && first != 'n');
-	int turn; // turn 0 = computer, turn 1 = player
 	(first == 'y') ? turn = 1 : turn = 0;
 	(first == 'y') ? userChar = 'X' : userChar = 'O';
 	(userChar == 'X') ? cpuChar = 'O' : cpuChar = 'X';
@@ -32,7 +40,7 @@ int main(void) {
 		scanf("%d", &difficulty);
 	} while (difficulty < 0 || difficulty > 3);
 	printf("\n%s", str);
-	while (gameIsWon() == 0) {
+	while (gameIsWon() == 0 && gameIsDrawn() == 0) {
 		if (turn == 0) {
 			int number;
 			do {
@@ -46,6 +54,7 @@ int main(void) {
 			} while (spotNotAvailable(number) == 1);
 			int index = indices[number - 1];
 			str[index] = cpuChar;
+			lastMove = number;
 			turn = 1;
 		} else {
 			int number;
@@ -55,6 +64,7 @@ int main(void) {
 			} while (number < 0 || number > 9 || spotNotAvailable(number) == 1);
 			int index = indices[number - 1];
 			str[index] = userChar;
+			lastMove = number;
 			turn = 0;
 		}
 		printf("\n%s", str);
@@ -82,6 +92,17 @@ int gameIsWon(void) {
 		return 1;
 	}
 	return 0;
+}
+
+int gameIsDrawn(void) {
+	for (int i = 0; i < 3; ++i) {
+		for (int j = 0; j < 3; ++j) {
+			if (board[i][j] == -1) {
+				return 0;
+			}
+		}
+	}
+	return 1;
 }
 
 int spotNotAvailable(int number) {
@@ -122,16 +143,75 @@ int spotNotAvailable(int number) {
 	return status;
 }
 
+// Random Generated Turn; Likely To Be Bad
 int takeEasyTurn(void) {
 	srand(time(NULL));
-	int value = (rand() % 9) + 1;
-	return value; 
+	int number = (rand() % 8) + 1;
+	return number;
 }
 
+// Uses Basic Logic To Formulate A Turn; Randomized Placement -> No Logic On Player Placement Accuracy
 int takeMediumTurn(void) {
+	if (spotNotAvailable(5) == 0) {
+		return 5;
+	} else if (userPlayedACorner() == 1) {
+		return turnBasedOnPlayedCorner();
+	} else if (userPlayedAnEdge() == 1) {
+
+	}
 	return 0;
 }
 
+int userPlayedACorner(void) {
+	if ((lastMove == 1 || lastMove == 3 || lastMove == 7 || lastMove == 9) && turn == 0 /* last turn was player */) {
+		return 1;
+	}
+	return 0;
+}
+
+int userPlayedAnEdge(void) {
+	if ((lastMove == 2 || lastMove == 4 || lastMove == 6 || lastMove == 8) && turn == 0 /* last turn was player */) {
+		return 1;
+	}
+	return 0;
+}
+
+int turnBasedOnPlayedCorner(void) {
+	if (lastMove == 1 && edgesOpen() == 1) {
+
+	} else if (lastMove == 1 && edgesOpen() == 0) {
+
+	} else if (lastMove == 3 && edgesOpen() == 1) {
+
+	} else if (lastMove == 3 && edgesOpen() == 0) {
+
+	} else if (lastMove == 7 && edgesOpen() == 1) {
+
+	} else if (lastMove == 7 && edgesOpen() == 0) {
+
+	} else if (lastMove == 9 && edgesOpen() == 1) {
+
+	} else /* lastMove == 9 && edgesOpen() == 0 */ {
+
+	}
+	return 0;
+}
+
+int edgesOpen(void) {
+	if (board[0][0] == -1 || board[0][2] == -1 || board[2][0] == -1 || board[2][2] == -1) {
+		return 1;
+	}
+	return 0;
+}
+
+int cornersOpen(void) {
+	if (board[0][1] == -1 || board[1][0] == -1 || board[1][2] == -1 || board[2][1] == -1) {
+		return 1;
+	}
+	return 0;
+}
+
+// Employs An Engine To Play A Turn
 int takeHardTurn(void) {
 	return 0;
 }
